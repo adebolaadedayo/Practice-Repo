c12f2b8c29dfc4fbd5e89e70e7f82091eedb6b7d
Adebola <adebolaadedayo21@gmail.com>
2026-02-07 17:38:11 +0000

    Comments were added to some code lines

--- Pythons/Test_Function.py
+++ Pythons/Test_Function.py
@@ -1,10 +1,16 @@
+#Create a function to print a list of names that are greater than 4 letters
+#Combine using a for loop and an if conditional statement
 def print_words(words):
     for w in words:
         if len(w) > 4:
-            print(w)
+            print(w) #the result is to get the words that fulfil the condition
+            
+#List of words
 words = ["Ability", "Man", "Adebola", "Apply"]
 
 print_words(words)
+
+#List of words with the variable "people"
 people = ["people", "attendance"]
 
 print_words(people)
