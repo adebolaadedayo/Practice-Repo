34b8989cd8fce0d94d840d5c59495e792093d30d
Adebola <adebolaadedayo21@gmail.com>
2026-02-07 17:22:14 +0000

    Tried my hands around to practice push function

---
+++ Pythons/Test_Function.py
@@ -0,0 +1,10 @@
+def print_words(words):
+    for w in words:
+        if len(w) > 4:
+            print(w)
+words = ["Ability", "Man", "Adebola", "Apply"]
+
+print_words(words)
+people = ["people", "attendance"]
+
+print_words(people)
