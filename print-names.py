aef0d28d0fa022626d83592c5c56b2429cf03b3c
Adebola <adebolaadedayo21@gmail.com>
2026-02-08 22:55:10 +0000

    Comments added

--- print-names.py
+++ print-names.py
@@ -1,9 +1,14 @@
+#A new function to print only actual names in a register
 def print_names(register):
     for names in register:
         if isinstance(names, str):
             print(names)
-
+#Register 1
 register = [1, 2, 3, "Ade", "Bola", "Joseph"]
 
 
 print_names(register)
+
+#Register 2
+new_register = ["Waliba", "Ahmed"]
+print_names(new_register)
